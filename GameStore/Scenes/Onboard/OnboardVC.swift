b945f45037482d5372216eabdd3c2db2aabe9486
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-16 13:28:12 +0300

    Add Onboarding Pod Files

--- GameStore/Scenes/Onboard/OnboardVC.swift
+++ GameStore/Scenes/Onboard/OnboardVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardVC: UIViewController {
 
     @IBOutlet weak var skipButton: UIButton!
     override func viewDidLoad() {
---
+++ Podfile
@@ -0,0 +1,8 @@
+# platform :ios, '9.0'
+
+target 'GameStore' do
+  use_frameworks!
+
+  # Pods for GameStore
+pod 'paper-onboarding'
+end
