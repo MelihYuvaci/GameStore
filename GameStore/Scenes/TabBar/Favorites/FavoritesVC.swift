56e0a37f2c42b4cb222e63fb9c6b80fc5156cdc1
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-16 14:04:21 +0300

    Add Tabbar Implantation and View Controllers

---
+++ GameStore/Scenes/TabBar/Favorites/FavoritesVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesVC.swift
+//  GameStore
+//
+//  Created by Melih Yuvacı on 16.01.2023.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
