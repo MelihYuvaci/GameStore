d26e67732ff12704cc945c301116728340ef1cc4
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-16 13:34:52 +0300

    Add Onboarding Screen Datasources and Delegates

---
+++ GameStore/Scenes/Home/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVC.swift
+//  GameStore
+//
+//  Created by Melih Yuvacı on 16.01.2023.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
