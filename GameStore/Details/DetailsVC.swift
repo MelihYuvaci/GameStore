db21eb85f9e0c980576d929f9d536f85403308b5
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-17 21:07:10 +0300

    Add Details VC

---
+++ GameStore/Details/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  GameStore
+//
+//  Created by Melih Yuvacı on 17.01.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
