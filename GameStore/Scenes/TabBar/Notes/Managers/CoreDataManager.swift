667a2e9a5e746ac2b3e2699d4c72097a8ae0f269
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-20 01:30:30 +0300

    Add Core Data Manager Manupulation Methods

--- GameStore/Scenes/TabBar/Notes/Managers/CoreDataManager.swift
+++ GameStore/Scenes/TabBar/Notes/Managers/CoreDataManager.swift
@@ -13,6 +13,69 @@ class CoreDataManager{
     
     private init (){}
     
+    func deleteTask(item : Notes, completion: @escaping (Bool) -> Void) {
+        let request: NSFetchRequest<Notes> = Notes.fetchRequest()
+        request.predicate = NSPredicate(format: "id = %@", item.id!.uuidString)
+        
+        do {
+            let context = persistentContainer.viewContext
+            let result = try context.fetch(request)
+            if result.count > 0 {
+                let item = result.first!
+                context.delete(item)
+                saveContext()
+                completion(true)
+            }
+        } catch let err {
+            print(err.localizedDescription)
+        }
+    }
+    
+    func completeTask(item : Notes, completion: @escaping (Bool) -> Void) {
+        let request: NSFetchRequest<Notes> = Notes.fetchRequest()
+        request.predicate = NSPredicate(format: "id = %@", item.id!.uuidString)
+        
+        do {
+            let result = try persistentContainer.viewContext.fetch(request)
+            if result.count > 0 {
+                let item = result.first!
+//                todo.completedTask = true
+//                todo.completedOn = Date()
+                saveContext()
+                completion(true)
+            }
+        } catch let err {
+            print(err.localizedDescription)
+        }
+    }
+    
+    func getAllTodos () -> [Notes] {
+        let request: NSFetchRequest<Notes> = Notes.fetchRequest()
+        let firstSort = NSSortDescriptor(key: #keyPath(Notes.name), ascending: true)
+        let secondSort = NSSortDescriptor(key: #keyPath(Notes.comment), ascending: false)
+        request.sortDescriptors = [firstSort, secondSort]
+        var items = [Notes]()
+        
+        do {
+            items = try persistentContainer.viewContext.fetch(request)
+        } catch let err {
+            print(err.localizedDescription)
+        }
+        
+        return items
+    }
+    
+    func saveToDo(name: String, comment: String, completion: @escaping (Bool) -> Void) {
+        let items = Notes(context: persistentContainer.viewContext)
+        items.id = UUID()
+        items.name = name
+        items.comment = comment
+        saveContext()
+        completion(true)
+    }
+    
+    //MARK: - Core Data stack
+    
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "DataModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
