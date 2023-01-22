0f66706a480b0bb1f4c874a900e3d4cf8a0381bb
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-22 16:56:25 +0300

    Add PickerViewHelper for HomeVC

---
+++ GameStore/Scenes/TabBar/Home/Helpers/HomeVCPickerViewHelper.swift
@@ -0,0 +1,94 @@
+//
+//  HomeVCPickerViewHelper.swift
+//  GameStore
+//
+//  Created by Melih Yuvacı on 22.01.2023.
+//
+
+import UIKit
+
+class HomeVCPickerViewHelper : NSObject{
+    
+    private weak var viewModel : HomeViewModel?
+    private weak var pickerViewButton : UIButton?
+    
+    let screenWidth = UIScreen.main.bounds.width - 10
+    let screenHeight = UIScreen.main.bounds.height / 3
+    var selectedRow = 0
+    
+    var games : KeyValuePairs =
+        [
+            "En Yeni Oyun" : "&ordering=created",
+            "En Eski Oyun" : "&ordering=-created",
+            "Popüler": ""
+        ]
+    
+    init(viewModel:HomeViewModel,pickerViewButton: UIButton) {
+        self.viewModel = viewModel
+        self.pickerViewButton = pickerViewButton
+        super.init()
+    }
+    
+    func popUpClicked()-> UIAlertController{
+        let vc = UIViewController()
+        vc.preferredContentSize = CGSize(width: screenWidth, height: screenHeight)
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height:screenHeight))
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        pickerView.selectRow(selectedRow, inComponent: 0, animated: false)
+        
+        vc.view.addSubview(pickerView)
+        pickerView.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor).isActive = true
+        pickerView.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor).isActive = true
+        
+        let alert = UIAlertController(title: "Select Filter", message: "", preferredStyle: .actionSheet)
+        
+        alert.popoverPresentationController?.sourceView = pickerViewButton
+        alert.popoverPresentationController?.sourceRect = pickerViewButton!.bounds
+        
+        alert.setValue(vc, forKey: "contentViewController")
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (UIAlertAction) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Select", style: .default, handler: { (UIAlertAction) in
+            self.selectedRow = pickerView.selectedRow(inComponent: 0)
+            let selected = Array(self.games)[self.selectedRow]
+            let filter = selected.value
+            self.viewModel!.viewDidLoad(filterText: filter)
+        }))
+        
+        return alert
+        
+    }
+}
+
+extension HomeVCPickerViewHelper: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView
+    {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 30))
+        label.text = Array(games)[row].key
+        label.sizeToFit()
+        return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat
+    {
+        return 60
+    }
+    
+}
+
+extension HomeVCPickerViewHelper : UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+        {
+            return 1 //return 2
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+        {
+            games.count
+        }
+}
+
--- GameStore/Scenes/TabBar/Home/View/HomeVC.swift
+++ GameStore/Scenes/TabBar/Home/View/HomeVC.swift
@@ -14,17 +14,8 @@ class HomeVC: UIViewController {
     
     private let viewModel = HomeViewModel()
     private var collectionHelper : HomeVCCollectionHelper!
+    private var pickerHelper : HomeVCPickerViewHelper!
     
-    let screenWidth = UIScreen.main.bounds.width - 10
-    let screenHeight = UIScreen.main.bounds.height / 3
-    var selectedRow = 0
-    
-    var games : KeyValuePairs =
-        [
-            "En Yeni Oyun" : "&ordering=created",
-            "En Eski Oyun" : "&ordering=-created",
-            "Popüler": ""
-        ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,65 +27,10 @@ class HomeVC: UIViewController {
     
     @IBAction func popUpPicker(_ sender: UIButton){
         
-        let vc = UIViewController()
-        vc.preferredContentSize = CGSize(width: screenWidth, height: screenHeight)
-        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height:screenHeight))
-        pickerView.dataSource = self
-        pickerView.delegate = self
-        
-        pickerView.selectRow(selectedRow, inComponent: 0, animated: false)
-        
-        vc.view.addSubview(pickerView)
-        pickerView.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor).isActive = true
-        pickerView.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor).isActive = true
-        
-        let alert = UIAlertController(title: "Select Filter", message: "", preferredStyle: .actionSheet)
-        
-        alert.popoverPresentationController?.sourceView = pickerViewButton
-        alert.popoverPresentationController?.sourceRect = pickerViewButton.bounds
-        
-        alert.setValue(vc, forKey: "contentViewController")
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (UIAlertAction) in
-        }))
-        
-        alert.addAction(UIAlertAction(title: "Select", style: .default, handler: { (UIAlertAction) in
-            self.selectedRow = pickerView.selectedRow(inComponent: 0)
-            let selected = Array(self.games)[self.selectedRow]
-            let filter = selected.value
-            let name = selected.key
-            self.viewModel.viewDidLoad(filterText: filter)
-        }))
-        
+        let alert = pickerHelper.popUpClicked()
         self.present(alert, animated: true, completion: nil)
-    }
-    
-}
 
-extension HomeVC: UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView
-    {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 30))
-        label.text = Array(games)[row].key
-        label.sizeToFit()
-        return label
     }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int
-    {
-        return 1 //return 2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
-    {
-        games.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat
-    {
-        return 60
-    }
-    
     
 }
 
@@ -102,6 +38,7 @@ private extension HomeVC{
     
     private func setupUI(){
         collectionHelper = .init(collectionView: collectionView, viewModel: viewModel, navigationController:navigationController!)
+        pickerHelper = .init(viewModel: viewModel,pickerViewButton: pickerViewButton)
     }
     
     func setupBindings(){
