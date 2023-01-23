bade88462d2b0fd57ab79438ad134db728d123a6
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-24 02:04:25 +0300

    Add AnimationVC and JsonFiles

---
+++ GameStore/Supporting Groups/Lottie/View/AnimationVC.swift
@@ -0,0 +1,40 @@
+//
+//  AnimationVC.swift
+//  GameStore
+//
+//  Created by Melih Yuvacı on 24.01.2023.
+//
+
+import UIKit
+import Lottie
+
+class AnimationVC: UIViewController {
+
+    @IBOutlet weak var animationView: LottieAnimationView!
+    
+    var jsonName : String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lottieAnimation()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    
+    func lottieAnimation(){
+        animationView = .init(name: jsonName ?? "")
+        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 700)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFit
+        view.addSubview(animationView)
+        animationView.play()
+        animationView.loopMode = .playOnce
+    }
+    func stopAnimation(){
+        animationView.stop()
+    }
+    
+}
