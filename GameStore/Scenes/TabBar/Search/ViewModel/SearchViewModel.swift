d449935a6c59b04c15b249ebbdd11c6c513aa1eb
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-24 22:32:49 +0300

    Fix Bug SearchViewModel

--- GameStore/Scenes/TabBar/Search/ViewModel/SearchViewModel.swift
+++ GameStore/Scenes/TabBar/Search/ViewModel/SearchViewModel.swift
@@ -21,7 +21,7 @@ class SearchViewModel{
     
     func viewDidLoad(searchText: String){
         isLoadingIndicatorShowing?(true)
-        model.fetchData(search: searchText)
+        model.fetchData(search:searchText.replacingOccurrences(of: " ", with: "-"))
     }
     
 }
